52858b68816f45a7c2b3f63ae2674bc77369b2b3
JSimple <josephinemayasimple@gmail.com>
2023-01-09 00:56:17 -0500

    add prisma, auth, some other stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,24 @@
-export default function Upload() {
-  return (
-    <>
-      <p>Upload a .png or .jpg image (max 1MB).</p>
-      <input
-        onChange={uploadPhoto}
-        type="file"
-        accept="image/png, image/jpeg"
-      />
-    </>
-  )
+import { NextPage } from 'next'
+import Table from 'react-bootstrap/Table'
+import Upload from './upload'
+
+
+const Index: NextPage = () =>{
+    return (
+        <div>
+            <Upload/>
+            <Table responsive striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Thumbnail</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+            </Table>
+        </div>
+    )
+
 }
 
-const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  const file = e.target.files?.[0]!
-  const filename = encodeURIComponent(file.name)
-  const fileType = encodeURIComponent(file.type)
-
-  const res = await fetch(
-    `/api/upload-url?file=${filename}&fileType=${fileType}`
-  )
-  const { url, fields } = await res.json()
-  const formData = new FormData()
-
-  Object.entries({ ...fields, file }).forEach(([key, value]) => {
-    formData.append(key, value as string)
-  })
-
-  const upload = await fetch(url, {
-    method: 'POST',
-    body: formData,
-  })
-
-  if (upload.ok) {
-    console.log('Uploaded successfully!')
-  } else {
-    console.error('Upload failed.')
-  }
-}
+export default Index
